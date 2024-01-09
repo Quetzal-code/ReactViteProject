11f63da2d1ab8282e83a413a8554929f954078b6
Quetzal <quetzaluzcategui@gmail.com>
2024-01-09 11:30:56 -0500

    Create ReactForms.jsx

---
+++ vite-project/src/learning/ReactForms.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App(){
+    const [name, setName] = useState ("");
+
+    return (
+        <div>
+            <form action="">
+                <fieldset>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor=""></label>
+                    </div>
+                    <button></button>
+                </fieldset>
+            </form>
+        </div>
+    );
+
+}
+
+export default App
